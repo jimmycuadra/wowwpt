b0f532ee39b14e554da0de0929519f8cf9e245ad
Jimmy Cuadra <jimmy@jimmycuadra.com>
2021-02-19 22:42:57 -0800

    wowat

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Container fluid>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Navigation() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>World of Warcraft Alt Tracker</Navbar.Brand>
+    </Navbar>
+  );
+}
