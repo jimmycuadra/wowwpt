8acb03cc7f0e39be4882ff0176c5a645f1612d25
Jimmy Cuadra <jimmy@jimmycuadra.com>
2021-02-22 17:46:43 -0800

    Change app name to WoW Weekly Progress Tracker.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 export default function Navigation() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>World of Warcraft Alt Tracker</Navbar.Brand>
+      <Navbar.Brand>World of Warcraft Weekly Progress Tracker</Navbar.Brand>
     </Navbar>
   );
 }
